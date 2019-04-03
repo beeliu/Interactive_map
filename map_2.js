22f299da118f316539fb5a9212dbb91f5e793b04
beeliu <belinda.liu@rocketmail.com>
2019-04-03 18:52:45 -0400

    Add files via upload

--- map_2.js
+++ map_2.js
@@ -1,4 +1,34 @@
-
+/* Map hilight functions*/
 $(function() {
     $('.nyc_map').maphilight();
+});
+
+$( "#manhattan_but" ).hover(function() {
+     var data = $('#Manhattan').mouseout().data('maphilight') || {};
+     data.alwaysOn = !data.alwaysOn;
+     $('#Manhattan').data('maphilight', data).trigger('alwaysOn.maphilight');
+});
+$( "#bronx_but" ).hover(function() {
+  var data = $('#Bronx').mouseout().data('maphilight') || {};
+  data.alwaysOn = !data.alwaysOn;
+  $('#Bronx').data('maphilight', data).trigger('alwaysOn.maphilight');
+
+});
+$( "#queens_but" ).hover(function() {
+  var data = $('#Queens').mouseout().data('maphilight') || {};
+  data.alwaysOn = !data.alwaysOn;
+  $('#Queens').data('maphilight', data).trigger('alwaysOn.maphilight');
+
+});
+$( "#brooklyn_but" ).hover(function() {
+  var data = $('#Brooklyn').mouseout().data('maphilight') || {};
+  data.alwaysOn = !data.alwaysOn;
+  $('#Brooklyn').data('maphilight', data).trigger('alwaysOn.maphilight');
+
+});
+$( "#island_but" ).hover(function() {
+  var data = $('#Staten_Island').mouseout().data('maphilight') || {};
+  data.alwaysOn = !data.alwaysOn;
+  $('#Staten_Island').data('maphilight', data).trigger('alwaysOn.maphilight');
+
 });
